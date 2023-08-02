dc0513fac34816fad7dac4d15b54e1dfbb6e5aa1
aliaslan01 <aslanmuhammedali047@gmail.com>
2023-08-02 16:18:06 +0300

    Yuze kadar tek sayilar
    Java ile 1'den 100'e kadar olan tek sayıları yazan proje

---
+++ yüze_kadar_tek_sayılar.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vize_hazirlik;
+
+
+public class yüze_kadar_tek_sayılar {
+    public static void main(String[] args) {
+        
+        
+        int i = 1;
+        while(i<100){
+            if(i%2==1){
+               System.out.println(i);
+            }
+            i++;
+            
+        }
+        
+        
+        
+    }
+}
